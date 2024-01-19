8704499b226a8326d6fecc5349c6e87d6fd7881d
bruno <brunofreire1717@gmail.com>
2024-01-19 00:24:18 -0500

    fix: showUserUseCase

--- src/modules/accounts/useCases/userUseCases/showUsers/showUserController.ts
+++ src/modules/accounts/useCases/userUseCases/showUsers/showUserController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { showUserUseCase } from "./showUserUseCase";
+import { showUsersUseCase } from "./showUserUseCase";
 import { container, injectable } from "tsyringe";
 
 @injectable()
@@ -7,7 +7,7 @@ class ShowUsersController {
     
     async handle(request: Request, response: Response, next: NextFunction): Promise<Response> {
         try {
-          const users = await showUserUseCase.execute();
+          const users = await showUsersUseCase.execute();
 
           return response.status(200).json(users);
         } catch (err) {
